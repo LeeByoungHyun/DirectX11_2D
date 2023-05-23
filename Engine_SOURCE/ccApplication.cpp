ca87e94e5f9c2fcd6256f758c7d2ccca6fc4bff9
Lee Byoung Hyun <leebh1126.0614@gmail.com>
2023-05-23 19:57:09 +0900

    feat : Direct11 윈도우 HWND에 연결
    Device, Context, SwapChain, RenderTarget, RenderTarget View, DepthStancil 생성

--- Engine_SOURCE/ccApplication.cpp
+++ Engine_SOURCE/ccApplication.cpp
@@ -18,6 +18,9 @@ namespace cc
 
 	void Application::Run()
 	{
+		Update();
+		LateUpdate();
+		Render();
 	}
 
 	void Application::Initialize()
@@ -34,6 +37,7 @@ namespace cc
 
 	void Application::Render()
 	{
+		graphicDevice->Draw();
 	}
 
 	void Application::SetWindow(HWND hwnd, UINT width, UINT height)
