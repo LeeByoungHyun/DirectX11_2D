7892e8b50daf05fcff3488b6d0ba9f1e3b883648
LeeByoungHyun <leebh1126.0614@gmail.com>
2023-06-19 13:21:44 +0900

    feat : modify Scene
    Add OnEnter, OnExit

--- Engine_SOURCE/ccSceneManager.cpp
+++ Engine_SOURCE/ccSceneManager.cpp
@@ -4,6 +4,7 @@
 namespace cc
 {
 	Scene* SceneManager::mActiveScene = nullptr;
+	std::map<std::wstring, Scene*> SceneManager::mScenes;
 
 	void SceneManager::Initialize()
 	{
@@ -24,5 +25,20 @@ namespace cc
 	void SceneManager::Render()
 	{
 		mActiveScene->Render();
+	}
+
+	Scene* SceneManager::LoadScene(std::wstring name)
+	{
+		std::map<std::wstring, Scene*>::iterator iter
+			= mScenes.find(name);
+
+		if (iter == mScenes.end())
+			return nullptr;
+
+		mActiveScene->OnExit();
+		mActiveScene = iter->second;
+		mActiveScene->OnEnter();
+
+		return iter->second;
 	}
 }
